ad7e407d094de056dd53de6f6d633fcc55210d6d
Sean Murphy <seanmurphy1661@gmail.com>
2020-05-06 20:58:28 -0400

    Rating Template and Color update

--- src/app/rating.component.ts
+++ src/app/rating.component.ts
@@ -1,50 +1,17 @@
-import { Component } from '@angular/core'
+import { Component, Input } from '@angular/core'
 
 @Component({
         selector: 'rating',
-        template: `
-            <i
-                class="glyphicon"
-                [class.glyphicon-star-empty]="rating < 1"
-                [class.glyphicon-star]="rating >= 1"
-                (click)="onClick(1)"
-            >
-            </i>
-            <i
-                class="glyphicon"
-                [class.glyphicon-star-empty]="rating < 2"
-                [class.glyphicon-star]="rating >= 2"
-                (click)="onClick(2)"
-            >
-            </i>
-            <i
-                class="glyphicon"
-                [class.glyphicon-star-empty]="rating < 3"
-                [class.glyphicon-star]="rating >= 3"
-                (click)="onClick(3)"
-            >
-            </i>
-            <i
-                class="glyphicon"
-                [class.glyphicon-star-empty]="rating < 4"
-                [class.glyphicon-star]="rating >= 4"
-                (click)="onClick(4)"
-            >
-            </i>
-            <i
-                class="glyphicon"
-                [class.glyphicon-star-empty]="rating < 5"
-                [class.glyphicon-star]="rating >= 5"
-                (click)="onClick(5)"
-            >
-            </i>
-
-
-        `
+        templateUrl: 'rating.component.html',
+        styles: [  `
+            .glyphicon-star { 
+                color:orange;
+            }
+        `]
 })
 
 export class RatingComponent{
-    rating = 0;
+    @Input() rating = 0;
 
     onClick(ratingValue){
         this.rating = ratingValue;
